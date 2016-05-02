5631a28eaf6f01597851cd79030d23a15bcecdcc
lozand <daniel_lozano@alumni.baylor.edu>
2016-05-02 00:32:38 -0500

    Added in a web forms project so that I can (hopefully) test these libraries a little easier, and maybe get some more work done. Also, I had to brush up on some of this web forms stuff.

---
+++ Portfolio_Manager.WebForm/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portfolio_Manager.WebForm
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddStock_Click(object sender, EventArgs e)
+        {
+            AddStock add = new AddStock(this);
+            add.Show();
+            this.Hide();
+        }
+    }
+}
