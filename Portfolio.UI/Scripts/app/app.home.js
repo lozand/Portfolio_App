2b785a319fd1eacff533105040ca82f430df58ad
lozand <daniel_lozano@alumni.baylor.edu>
2016-05-02 00:02:03 -0500

    Added in a web project so that the base library can be accessed. The web project is not all the way functional. Still trying to setup the JS structure. I will definitely need a bit of help here. In the meantime, I think I will create a webforms app so I can test everything. That will be way easier to setup. I figure once I can get all the base stuff working, I can open up the web side

---
+++ Portfolio.UI/Scripts/app/app.home.js
@@ -0,0 +1,14 @@
+﻿var app = app || {};
+
+app.home = {
+    index: function () {
+        alert('it worked');
+        app.home.loadButtons();
+    },
+    loadButtons: function () {
+        $('.js-say-hi').on('click', function (e) {
+            e.preventDefault();
+            alert('hi');
+        });
+    }
+};
---
+++ Portfolio.UI/Scripts/app/app.js
@@ -0,0 +1,7 @@
+﻿var app = app || {};
+(function () {
+    var container = $('.content-container')[0];
+    var vmName = container.id;
+    var vmToRun = 'app.' + vmName + '.index()';
+    eval(vmToRun);
+})();
