c04c2c3b9eee4f638dad411c165a2ee51774366c
lozand <daniel_lozano@alumni.baylor.edu>
2016-05-29 13:09:34 -0500

    Oops.
    Definitely should have made sure that this could build before I
    committed

--- Portfolio.UI/Controllers/PortfolioController.cs
+++ Portfolio.UI/Controllers/PortfolioController.cs
@@ -46,10 +46,5 @@ namespace Portfolio.UI.Controllers
             }
             return Json(new HttpStatusCodeResult(System.Net.HttpStatusCode.InternalServerError, "You need to be signed in to use this page"), JsonRequestBehavior.AllowGet);
         }
-
-        public JsonResult BuyStock(int stockId, int quantity)
-        {
-
-        }
     }
 }
